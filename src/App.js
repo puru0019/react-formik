6111d211abec09002aa268dd0226bf14cef84cf4
Gopi Gokaraju <gopi.g@COMAKEIT.COM>
2019-01-31 17:42:18 +0530

    adding debug component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 import './App.css';
-import Landing from './formik/Landing'
+import Landing from './formik/Landing';
+import Navigation from './Navigation';
+import FormikComponent from './formik/FormikComponent';
 
 class App extends Component {
   render() {
@@ -10,7 +12,11 @@ class App extends Component {
       
         <Router>
             <div>
-              <Route path="/" exact component={Landing} />
+              <Navigation/>
+              <div>
+                <Route path="/" exact component={Landing} />
+                <Route path="/formik" exact component={FormikComponent} />
+              </div>
             </div>
         </Router>
       
---
+++ src/formik/FormikComponent.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { compose,  } from 'recompose';
+import { Formik, Form, Field } from 'formik';
+import { Debug } from './Debug';
+import { Button, Row, Col } from 'react-bootstrap';
+
+const formInitialValues = {fname: '', lname: ''};
+
+const enhance = compose();
+
+const FormikComponent = enhance(() => {
+    return (
+        <React.Fragment>
+            <Formik
+            initialValues={{ ...formInitialValues }}
+            onSubmit={ (values, action) => {
+                console.log(values,action);
+                action.setSubmitting(false);
+            }}
+            >
+                {
+                    ({ isSubmitting, errors, dirty }) => 
+                    <div className="container">
+                       <h3>Simple Formik</h3>
+                       <Form>
+                           <Row>
+                           <Col sm="4">
+                            <div className="form-group">
+                                <Field type="text" name="fname" className="form-control" placeholder="Enter First Name" />
+                            </div>
+                            <div className="form-group">
+                                <Field type="text" name="lname" className="form-control" placeholder="Enter Last Name" />
+                            </div>
+                           </Col>
+                           </Row>
+                           <Button color="Success" type="submit" disabled={isSubmitting || !dirty}>Submit</Button>
+                           <Debug />
+                       </Form> 
+                    </div>
+                    
+                }
+            </Formik>
+        </React.Fragment>
+    )
+});
+
+export default FormikComponent
